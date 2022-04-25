b164d21b4b962c23d50b2d3a8de85c6e062effcc
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-25 22:48:20 +0700

    Memek

--- plugins/start.py
+++ plugins/start.py
@@ -122,8 +122,8 @@ async def start_command(client: Client, message: Message):
                 InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close"),
             ],
         ]
-       await message.reply_text(
-           text=START_MSG.format(
+        await message.reply_text(
+            text=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None
