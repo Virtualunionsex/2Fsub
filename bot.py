f80a35a8d2bb3ea9c60a84bb8e65b16a52ba7a18
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-25 21:57:27 +0700

    .

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
 # (©)Codexbotz
-# Recode By @Mafia_Tobatz
-# Kalo clone Gak usah hapus 
-# gue tandain akun tele nya ngentod
 
 import pyromod.listen
 import sys
@@ -14,7 +11,6 @@ from config import (
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
-    FORCE_SUB_CHANNEL1,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -65,22 +61,6 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
-                )
-                sys.exit()
-        if FORCE_SUB_CHANNEL1:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNEL1!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
                     "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
